e3c2768bf18ce709776e9d8e2d8036cba5c2ab6f
KadeHennacy <kade.hennacy@gmail.com>
2023-02-21 11:36:36 -0500

    Updated metadata and package names to fit the project

--- src/main/java/com/kade/mcps/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.kade.mcps.service;
-
-import com.kade.mcps.payload.AuthRequestDto;
-
-public interface AuthService {
-    String authenticate(AuthRequestDto request);
-
-    String refresh(String token);
-}
