bccbe09447d383c5f18001dd922756597f509109
KadeHennacy <kade.hennacy@gmail.com>
2023-02-21 12:37:11 -0500

    Homepage prototype done

--- src/main/frontend/src/scenes/landingPage/index.js
+++ src/main/frontend/src/scenes/landingPage/index.js
@@ -1,30 +1,70 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom/dist";
-import { useSelector, useDispatch } from "react-redux";
-import { logout } from "../../store/reducers/auth";
-import axios from "../../api/axios";
-import { Box } from "@mui/material";
+import React from "react";
+import styled from "@emotion/styled";
 
-export default function LandingPage() {
-  const [greeting, setGreeting] = useState("");
-  const token = useSelector((state) => state.auth.token);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+const Hero = styled.div`
+  background-image: url("banner.png");
+  background-size: cover;
+  background-position: center;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); ;
+`;
+const Title = styled.h1`
+  color: white;
+  font-size: 4rem; ;
+`;
 
-  const getGreeting = async () => {
-    const jwt = token;
-    const res = await axios.get("/api/v1/greetings", {
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    });
-    setGreeting(res.data);
-  };
+const Description = styled.p`
+  color: white;
+  font-size: 2rem; ;
+`;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem; ;
+`;
+
+const Button = styled.button`
+  margin: 0 1rem;
+  padding: 1rem 2rem;
+  font-size: 1.5rem;
+  background-color: #0091ea;
+  color: white;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: translateY(-3px);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+  }
+
+  &:active {
+    transform: translateY(1px);
+    box-shadow: none;
+  }
+`;
+
+const LandingPage = () => {
   return (
-    <Box m={3}>
-      <div>Landing Page Page</div>
-      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
-    </Box>
+    <div>
+      <Hero>
+        <Title>Character Builder</Title>
+        <Description>Lorem Ipsom Blah Blah Blah</Description>
+        <ButtonContainer>
+          <Button>Login</Button>
+          <Button>Create Account</Button>
+          <Button>Create Character as Guest</Button>
+        </ButtonContainer>
+      </Hero>
+    </div>
   );
-}
+};
+
+export default LandingPage;
